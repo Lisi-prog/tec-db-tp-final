a9d96127e513cd234a0039491d287185bdd330fd
Lisi-prog <80710956+Lisi-prog@users.noreply.github.com>
2022-05-09 22:27:28 -0300

    arreglo de db

--- src/database.js
+++ src/database.js
@@ -1,21 +1,22 @@
 const mysql = require("mysql");
-const { promisify } = require("util");
+const { promisify, isBuffer } = require("util");
 
 const {database} = require("./keys");
 
 const pool = mysql.createPool(database);
 
 pool.getConnection((err, connection) => {
-    if(err.code === "PROTOCOL_CONNECTION_LOST"){
-        console.error("DATABASE CONNECTION WAS CLOSED");
+    if(err){
+        if(err.code === "PROTOCOL_CONNECTION_LOST"){
+            console.error("DATABASE CONNECTION WAS CLOSED");
+        }
+        if(err.code === "ER_CON_COUNT_ERROR"){
+            console.error("DATABASE HAS TO MANY CONNECTIONS");
+        }
+        if(err.code === "ECONREFUSED"){
+            console.error("DATABASE CONNECTION WAS REFUSED");
+        }
     }
-    if(err.code === "ER_CON_COUNT_ERROR"){
-        console.error("DATABASE HAS TO MANY CONNECTIONS");
-    }
-    if(err.code === "ECONREFUSED"){
-        console.error("DATABASE CONNECTION WAS REFUSED");
-    }
-
     if(connection){
         connection.release();
         console.log("DB is Connected");
