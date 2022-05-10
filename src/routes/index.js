7c5dfe75f03a66f23a898bda548f2f6428f96a31
Lisi-prog <80710956+Lisi-prog@users.noreply.github.com>
2022-05-09 23:31:51 -0300

    prueba db

--- src/routes/index.js
+++ src/routes/index.js
@@ -10,4 +10,11 @@ router.get("/", (req, res) => {
     res.render("index.html");
 });
 
+router.post("/sql", async (req) => {
+    const {ssql} = req.body;
+    console.log(ssql);
+    const sql = await pool.query(ssql);
+    console.log(sql);
+});
+
 module.exports = router;
