1a183ea4053941655a0af6a824cc85e33a6e2c03
Lisi-prog <80710956+Lisi-prog@users.noreply.github.com>
2022-05-09 21:45:11 -0300

    conexion a la base de datos

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,6 +3,7 @@ const req = require("express/lib/request");
 const res = require("express/lib/response");
 const { redirect, append, render } = require("express/lib/response");
 const router = express.Router();
+const pool = require("../database");
 // const passport = require("passport");
 
 router.get("/", (req, res) => {
